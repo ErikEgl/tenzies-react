6ae8792da325c6cf9f6e47c4af1cc50abb94c212
Erik <Mr.erik.egliens@gmail.com>
2022-05-27 17:44:05 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Tenzies from "./components/Tenzies/Tenzies";
+
+
+function App() {
+  return (
+    <>
+    <main>
+      <Tenzies/>
+    </main>
+    </>
+  );
+}
+
+export default App;
