a5464484d7033537c5628fde22136631bf73ddef
Erik <Mr.erik.egliens@gmail.com>
2022-05-29 15:08:07 +0300

    added die functionality for changing state

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import Die from "./components/Die/Die";
 function allNewDice() {
   let diceValArray = [];
   for (let i = 0; i < 10; i++) {
-    diceValArray.push(Math.ceil(Math.random() * 6));
+    diceValArray.push({
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: i,
+    });
   }
   return diceValArray;
 }
@@ -12,14 +16,37 @@ function allNewDice() {
 function App() {
   const [arrayOfNumbers, setArrayOfNumbers] = React.useState(allNewDice);
   const dieComponent = arrayOfNumbers.map((num, index) => {
-    return <Die value={num} key={index} />;
+    return (
+      <Die
+        holdDice={(id) => holdDice(num.id)}
+        value={num.value}
+        key={num.id}
+        isHeld={num.isHeld}
+      />
+    );
   });
+
+  
+  function holdDice(id) {
+    setArrayOfNumbers(prevArray => {
+      return prevArray.map(prevArrayNum => {
+        return prevArrayNum.id === id ? { ...prevArrayNum, isHeld: !prevArrayNum.isHeld } : prevArrayNum;
+      });
+    });
+  }
+
+  function clickHandler() {
+    return setArrayOfNumbers(allNewDice);
+  }
+
   return (
     <>
       <main>
         <div className="wrap">
           {dieComponent}
-          <button className="roll-btn">Roll Dice</button>
+          <button onClick={clickHandler} className="roll-btn">
+            Roll Dice
+          </button>
         </div>
       </main>
     </>
