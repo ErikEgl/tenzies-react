42e21ff196180738992301b2f93b75eb66d237ec
Erik Egliens <erik@bellcom.ee>
2022-10-11 18:57:07 +0300

    added dots

--- src/components/Die/Die.js
+++ src/components/Die/Die.js
@@ -4,7 +4,11 @@ function Die(props) {
   return (
     <>
       <div className="die">
-        <button onClick={props.holdDice} className={props.isHeld === true ? 'die-num is-held' : 'die-num'}>{props.value}</button>
+        <button onClick={props.holdDice} className={`die-value-${props.value} ${props.isHeld === true ? 'die-num is-held' : 'die-num'}`}>
+        {[...Array(props.value)].map((value, index) => (
+            <div key={index} id={index + 1} className={`dot dot-${index + 1}`}></div>
+          ))}
+        </button>
       </div>
     </>
   );
